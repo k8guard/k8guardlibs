a28398436cbe8faa917582063d6d1fc8a839a8f5
medyagh <medya.ghazizadeh@target.com>
2017-07-12 15:45:14 -0500

    use kube config if it is in the path

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -3,11 +3,22 @@ package k8s
 import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
 )
 
 
 func LoadClientset() (*kubernetes.Clientset, error) {
-	config, err := rest.InClusterConfig()
+
+	var config *rest.Config
+	var err error
+
+	if _, err := os.Stat("/root/.kube/config"); os.IsNotExist(err) {
+		config, err = rest.InClusterConfig()
+	} else {
+		config, err = clientcmd.BuildConfigFromFlags("", "/root/.kube/config")
+
+	}
 	if err != nil {
 		return &kubernetes.Clientset{}, err
 	}
