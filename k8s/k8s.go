97b4e59ffdfde14a1c8f3def87c51dfa1a2201d5
medyagh <medya.ghazizadeh@target.com>
2017-07-12 16:14:33 -0500

    incluster outcluster config fix

--- k8s/k8s.go
+++ k8s/k8s.go
@@ -2,23 +2,20 @@ package k8s
 
 import (
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
 )
 
-
 func LoadClientset() (*kubernetes.Clientset, error) {
 
-	var config *rest.Config
-	var err error
+	configFilePath := "/root/.kube/config"
 
 	if _, err := os.Stat("/root/.kube/config"); os.IsNotExist(err) {
-		config, err = rest.InClusterConfig()
-	} else {
-		config, err = clientcmd.BuildConfigFromFlags("", "/root/.kube/config")
-
+		configFilePath = ""
 	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", configFilePath)
+
 	if err != nil {
 		return &kubernetes.Clientset{}, err
 	}
@@ -28,4 +25,3 @@ func LoadClientset() (*kubernetes.Clientset, error) {
 	}
 	return clientset, nil
 }
-
